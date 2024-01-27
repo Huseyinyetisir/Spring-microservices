e62d447bdb189827397e36f78209b133ec9bb20d
Hüseyin Yetişir <huseynyetisir@gmail.com>
2024-01-27 20:55:03 +0300

    Created

---
+++ microservice-doodles/src/main/java/com/microservicedoodles/microservicedoodles/MicroserviceDoodlesApplication.java
@@ -0,0 +1,13 @@
+package com.microservicedoodles.microservicedoodles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDoodlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDoodlesApplication.class, args);
+	}
+
+}
---
+++ microservice-doodles/src/test/java/com/microservicedoodles/microservicedoodles/MicroserviceDoodlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicedoodles.microservicedoodles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDoodlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
